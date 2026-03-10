4688d03bedd3774569470a5c3751feb3ba5c6f83
Bode <289588@burlcoschools.org>
2026-03-10 01:11:59 -0400

    Update weatherprogram.cpp

--- weatherprogram.cpp
+++ weatherprogram.cpp
@@ -3,21 +3,35 @@
 using namespace std;
 
 double wind, temp, dew;
-double chill = 35.74 + 0.6215 * temp - 37.75 * pow(wind, 0.16) + pow(0.4275 * temp * wind, 0.16);
-double cloud = temp - dew / 2.5 * 1000;
 
 void weatherOutput() {
+    cout << "____________________________________________________________________________________________________\n";
+    cout << "|  Temperature (F)  |  Wind Speed (Mph)  |  Dew Point (F)  |  Wind Chill  (F)  |  Cloud Base (Ft)  |\n";
     cout << "|==================================================================================================|\n";
-    cout << "|  Temperature (F)  |  Wind Speed (Kts)  |  Dew Point (F)  |  Wind Chill  (F)  |  Cloud Base (Ft)  |\n";
-    cout << "|==================================================================================================|\n";
-    cout << temp << "        " << wind << "        " << dew << "       " << chill << "      " << temp - dew / 2.5 * 1000 << "\n";
+    cout << "|  " << temp << " degrees       |  " << wind << " mph             |  " << dew << " degrees      |  " << 35.74 + (0.6215 * temp) - (37.75 * pow(wind, 0.16)) + pow(0.4275 * temp * wind, 0.16) << " degrees  |  " << 1000 * ((temp - dew) / 2.5) << " feet       |\n";
+    cout << "|___________________|____________________|_________________|___________________|___________________|";
 }
 
 int main()
 {
-    cin >> wind;
-    cin >> temp;
-    cin >> dew;
+    cout << "Enter the temperature in Farenheit: \n";
+        cin >> temp;
+    cout << "Enter the wind speed in mph: \n";
+        cin >> wind;
+    cout << "Enter the dewpoint in Fahrenheit: \n";
+        cin >> dew;
+
+    //denial
+    if (!temp) {
+        cout << "Please enter your temperature again: \n";
+            cin >> temp;
+        weatherOutput();
+    } 
+    if (!wind) {
+        cout << "Please enter your wind speed again: \n";
+            cin >> wind;
+        weatherOutput();
+    }
     weatherOutput();
     return 0;
 }
